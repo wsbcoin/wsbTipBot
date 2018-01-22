20792f659f9bf11f2112ca32ecfe0bc3783a6243
Theriley106 <christopherlambert106@gmail.com>
2018-01-22 13:54:46 -0500

    Added username and b64 password to bot

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 import praw
+import base64
+
+USERNAME = "WSBTip"
+PASSWORD = base64.b64decode(open("../../Password.txt").read())
+# Not going to post the password in plain text
 
 class bot(object):
 	def __init__(self, username, password):
 		self.username = username
 		self.password = password
+
+
+
+if __name__ == '__main__':
+	WSBbot = bot(USERNAME, PASSWORD)
